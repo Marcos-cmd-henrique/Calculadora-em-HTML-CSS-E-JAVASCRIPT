bc1e16cd1807bbdfac92effdde8dc0f8c631f03e
Marcos Henrique <marcossistemas.ac@gmail.com>
2022-11-04 17:41:44 -0500

    Css e javascript

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,52 +1,31 @@
-const formData = new FormData(document.querySelector('form'))
-const inputValue = formData.get("name")
-console.log(inputValue) 
+//const formData = new FormData(document.querySelector('form'))
+//const inputValue = formData.get("name")
+//console.log(inputValue) 
 
-
-function soma() {
-   //alert("1");
-   var f=document.getElementById("frm");
-   var n1=document.getElementById("n1").value;
-   var n2=document.getElementById("n2").value;
-   var soma = n1 + n2 ;
-   alert(soma);
+function insert(num)
+{
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
 }
-
-function subtracao() {
-   var f=document.getElementById("frm");
-   var n1=document.getElementById("n1").value;
-   var n2=document.getElementById("n2").value;
-   var soma = n1 - n2 ;
-   alert(soma);
-} 
-
-function multiplicacao() {
-   var f=document.getElementById("frm");
-   var n1=document.getElementById("n1").value;
-   var n2=document.getElementById("n2").value;
-   var soma = n1 * n2 ;
-   alert(soma);
-} 
-
-function divisao() {
-   var f=document.getElementById("frm");
-   var n1=document.getElementById("n1").value;
-   var n2=document.getElementById("n2").value;
-   var soma = n1 / n2 ;
-   alert(soma);
-} 
-
-function mostrarresultados() {
-    console.log(mostrarresultados)
-
+function clean()
+{
+    document.getElementById('resultado').innerHTML = "";
 }
-
-function limpar(){
-   //alert("1");
-   var f=document.getElementById("frm");
-   var n1 = f.n1;
-   var n2 = f.n2;
-
-   n1.value = "";
-   n2.value = "";
+function back()
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
 }
+function calcular()
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado)
+    {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else
+    {
+        document.getElementById('resultado').innerHTML = "Nada..."
+    }
+}
+﻿
