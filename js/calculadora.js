7e5cbd309c3ae0eeea35c0bcd4deffd7cb5c8b71
Marcos Henrique <marcossistemas.ac@gmail.com>
2022-11-04 17:52:52 -0500

    Remoção do html de teste e js de teste da aula

--- js/calculadora.js
+++ js/calculadora.js
@@ -4,7 +4,7 @@
 
 function insert(num)
 {
-    var numero = document.getElementById('resultado').innerHTML;
+    let numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
 }
 function clean()
@@ -13,12 +13,12 @@ function clean()
 }
 function back()
 {
-    var resultado = document.getElementById('resultado').innerHTML;
+    let resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
 }
 function calcular()
 {
-    var resultado = document.getElementById('resultado').innerHTML;
+    let resultado = document.getElementById('resultado').innerHTML;
     if(resultado)
     {
         document.getElementById('resultado').innerHTML = eval(resultado);
@@ -28,4 +28,3 @@ function calcular()
         document.getElementById('resultado').innerHTML = "Nada..."
     }
 }
-﻿
